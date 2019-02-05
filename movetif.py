24a7e152be077a8277e1ae60df4b2f30cb90371d
Daria Koro <daria.korotayeva@yandex.ru>
2019-02-05 18:56:39 +0200

    old useful stuff

---
+++ movetif.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+
+
+path = 'D:\\Daria\\uni\\MS'
+
+
+folders = os.listdir(path)
+for folder in folders:
+    os.makedirs(os.path.join(path, folder, 'tif'))
+    files = os.listdir(os.path.join(path, folder))
+    for file in files:
+        if file[-4:] == '.tif':
+            os.rename(os.path.join(path, folder, file), os.path.join(path, folder, 'tif', file))
+            #print(file)
+    print(folder)
